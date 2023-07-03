eb3b39f5cf8a5dce8e1ea5d3495104305327b4b9
mukesh <mukeshmehta2041@gmail.com>
2023-07-03 11:32:31 +0530

    deleting a task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [todoList, setTodoList] = useState("");
+  const [todoList, setTodoList] = useState([]);
   const [currentTask, setCurrentTask] = useState("");
 
+  const inputTask = useRef(null);
+
   const addTask = () => {
+    if (currentTask.trim("").length === 0) return;
     setTodoList([...todoList, currentTask]);
+    inputTask.current.value = "";
+    setCurrentTask("");
+  };
+
+  const deleteTask = (value) => {
+    const updatedList = todoList.filter((task) => task !== value);
+    setTodoList(updatedList);
   };
 
   return (
     <div className="App">
       <h1>Todo List</h1>
       <input
+        ref={inputTask}
         type="text"
         placeholder="Task..."
         onChange={(e) => {
@@ -23,7 +34,12 @@ function App() {
       <hr />
       <ul>
         {todoList.map((value, index) => {
-          return <li key={index}>{value}</li>;
+          return (
+            <div className="task">
+              <li key={index}>{value}</li>
+              <button onClick={() => deleteTask(value)}> Delete</button>
+            </div>
+          );
         })}
       </ul>
     </div>
