ea5cfa63576a6a39bb4ed65d729c88989cc4a59b
mukesh <mukeshmehta2041@gmail.com>
2023-07-03 09:57:53 +0530

    add a task to the Todo List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todoList, setTodoList] = useState("");
+  const [currentTask, setCurrentTask] = useState("");
+
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+    console.log(todoList);
+  };
+
   return (
     <div className="App">
       <h1>Todo List</h1>
-      <input type="text" placeholder="Task..." />
-      <button>Add Tak</button>
+      <input
+        type="text"
+        placeholder="Task..."
+        onChange={(e) => {
+          setCurrentTask(e.target.value);
+        }}
+      />
+      <button onClick={addTask}>Add Tak</button>
       <hr />
     </div>
   );
